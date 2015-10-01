4679548d7f5cb9a40d71e7bf0ee4290283503b3d
toga84 <toga.desarrollo@gmail.com>
2015-10-01 13:14:20 +0200

    Contador_JS

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es-ES" lang="es-ES">
+
+  <head>
+    <title>Cuadro flotante</title>
+    <meta charset="utf-8">    
+  </head>
+
+  <body>
+
+  <?php
+    include('cuadro.html');
+  ?>
+
+
+  </body>  
+</html>  
